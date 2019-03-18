07728faee546ba836456e11ea89fa3f9097315eb
mmaher95 <R3V@R3Vs-MacBook-Pro.local>
2019-03-18 13:31:11 +0200

    Initial Commit

---
+++ InstaMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaMovies
+//
+//  Created by Mina Maher on 3/18/19.
+//  Copyright © 2019 Mina Maher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
